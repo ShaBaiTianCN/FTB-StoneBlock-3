9ac3ac150831564e027fae0ec0d46700d9fc3a84
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-11-04 12:45:20 +0000

    [T] (kubejs) easter_eggs (#3)
    Co-authored-by: Cactusstudent <97865546+Cactusstudent@users.noreply.github.com>

--- kubejs/server_scripts/easter_eggs.js
+++ kubejs/server_scripts/easter_eggs.js
@@ -10,16 +10,16 @@ onEvent("player.logged_in", (event) => {
     currentDate.getDate(); //[0,31]
   //Syntax, max is [12-31]
   let birthdays = [
-    ["Gaz", "3-9", "Mod Developer"],
-    ["OfficialyMax", "3-10", "Quest writer"],
-    ["Dinnerbeef & UnRealDinnerbone", "6-8", "Alpha testing"],
-    ["Slowpoke101", "6-19", "Our overlord"],
-    ["manmaed", "6-30", "Alpha testing"],
-    ["Loneztar", "7-15", "Beta team leader"],
-    ["Mikey", "10-11", "Lead Mod Developer"],
-    ["5ully_", "10-15", "Alpha testing"],
-    ["Sunekaer", "10-20", "Lead Modpack Developer"],
-    ["aaronhowser1", "12-2", "Quest writer"],
+    ["Gaz", "3-9", "模组开发"],
+    ["OfficialyMax", "3-10", "任务撰写员"],
+    ["Dinnerbeef & UnRealDinnerbone", "6-8", "Alpha版测试"],
+    ["Slowpoke101", "6-19", "总书记"],
+    ["manmaed", "6-30", "Alpha版测试"],
+    ["Loneztar", "7-15", "Beta团队主任"],
+    ["Mikey", "10-11", "模组开发主任"],
+    ["5ully_", "10-15", "Alpha版测试"],
+    ["Sunekaer", "10-20", "整合包开发主任"],
+    ["aaronhowser1", "12-2", "任务撰写员"],
   ];
 
   birthdays.forEach((birthday) => {
@@ -28,8 +28,8 @@ onEvent("player.logged_in", (event) => {
 
     if (birthdayDate == today) {
       player.tell([
-        Component.string("Happy birthday to "),
-        Component.string(birthdayPerson + "!")
+        Component.string("生日快乐，"),
+        Component.string(birthdayPerson + "！")
           .underlined()
           .hover(Component.string(birthday[2])),
       ]);
