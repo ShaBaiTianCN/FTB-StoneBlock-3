da7b3b2cebc779faf3b730089689ed38a3497d69
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-11-22 14:44:02 +0000

    [T] (kubejs) info_tabs
    Co-authored-by: Cactusstudent <97865546+Cactusstudent@users.noreply.github.com>
    Co-authored-by: 奈 <58922212@qq.com>
    Co-authored-by: 海福 <18917570841@163.com>
    Co-authored-by: Rasilia-冬风 <2634779867@qq.com>
    Co-authored-by: KlparetlR <3421037143@qq.com>
    Co-authored-by: 洛 <104070571+luo-2233@users.noreply.github.com>
    Co-authored-by: Zicai <25471437+13584452567@users.noreply.github.com>

--- kubejs/client_scripts/info_tabs.js
+++ kubejs/client_scripts/info_tabs.js
@@ -1,86 +1,86 @@
 onEvent('rei.information', (event) => {
 	event.addItem(['ftbstoneblock:stonepebble'], 'Pebbles', [
-		'Break Stone with your hand to get Stone Pebbles',
+			'空手破坏石头以获得圆石石子。',
 	])
 	event.addItem('ftbstoneblock:mob_soul', 'Souls', [
-		'Has an 80% chance of spawning every time a mob dies.',
+			'在怪物死亡时有80%的概率掉落。',
 	])
 	event.addItem('ftbstoneblock:ritual_starter', 'Rituals', [
-		'Can be found in End City structures.',
+			'能够在末地建筑中找到。',
 	])
 	event.addItem(
-		'ftbstoneblock:cold_silverfish_shard',
-		'Cold Silverfish Shard',
-		['Can be found in dungeons in the stone area.']
+			'ftbstoneblock:cold_silverfish_shard',
+			'Cold Silverfish Shard',
+			['可在石头区域的地牢中找到。']
 	)
 	event.addItem(
-		'ftbstoneblock:warm_silverfish_shard',
-		'Warm Silverfish Shard',
-		['Can be found in dungeons in the nether area.']
+			'ftbstoneblock:warm_silverfish_shard',
+			'Warm Silverfish Shard',
+			['可在地狱区域的地牢中找到。']
 	)
 	event.addItem(
-		'ftbstoneblock:silverfish_heart',
-		'Silverfish Heart',
-		[
-			'Drops from the Mother Silverfish',
-			' ',
-			'Summon in the Boss Dungeon beyond the End Ring'
-		]
-	)
-
-	event.addItem(
-		'mob_grinding_utils:mob_swab_used',
-		'Used Mob Swab',
-		[
-			'Use a Mob Swab on a mob'
-		]
+			'ftbstoneblock:silverfish_heart',
+			'Silverfish Heart',
+			[
+					'由蠹虫妈妈掉落',
+					' ',
+					'在末地环中的Boss地牢中生成'
+			]
 	)
 
 	event.addItem(
-		'mob_grinding_utils:golden_egg',
-		'Golden Egg',
-		[
-			'Feed Nutritious Chicken Feed to a Chicken'
-		]
+			'mob_grinding_utils:mob_swab_used',
+			'Used Mob Swab',
+			[
+					'对一个怪物使用怪物棉签。'
+			]
 	)
 
 	event.addItem(
-		'mob_grinding_utils:gm_chicken_feed_cursed',
-		'GM Chicken Feed Cursed',
-		[
-			'Feed GM Chicken Feed Cursed to a Chicken'
-		]
+			'mob_grinding_utils:golden_egg',
+			'Golden Egg',
+			[
+					'给鸡喂食营养丰富鸡饲料。'
+			]
+	)
+
+	event.addItem(
+			'mob_grinding_utils:gm_chicken_feed_cursed',
+			'GM Chicken Feed Cursed',
+			[
+					'给鸡喂食GM鸡饲料。'
+			]
 	)
 	
 	event.addItem(
-		[
-			'ars_nouveau:starbuncle_shards',
-			'ars_nouveau:whirlisprig_shards',
-			'ars_nouveau:drygmy_shard'
-		],
-		'Ars Shards',
-		[
-			'While normally biome-specific, they\'ve been set to spawn in Plains like other animals',
-			' ',
-			'See the Worn Notebook for more information'
-		]
+			[
+					'ars_nouveau:starbuncle_shards',
+					'ars_nouveau:whirlisprig_shards',
+					'ars_nouveau:drygmy_shard'
+			],
+			'Ars Shards',
+			[
+					'虽然通常是生物群系特有的，但它们被设置为像其它动物一样在平原上生成',
+					' ',
+					'在破旧的笔记本中查看更多信息'
+			]
 	)
 
 	event.addItem('bloodmagic:weak_tau',
-		'Weak Tau',
-		[
-			'Found in Simple Dungeons (see "The Antechamber" in the Sanguine Scientem)',
-			' ',
-			'Can be farmed on Farmland',
-		]
+			'Weak Tau',
+			[
+					'可以在恶魔领域中找到（请查阅血染知书中的“The Antechamber”）',
+					' ',
+					'可以在耕地上种植',
+			]
 	)
 
 	event.addItem('bloodmagic:strong_tau',
-		'Strong Tau',
-		[
-			'Can be rarely found in Simple Dungeons (see "The Antechamber" in the Sanguine Scientem)',
-			' ',
-			'If normal Tau Fruit grows while an animal is standing on top, it grows into Saturated Tau instead',
-		]
+			'Strong Tau',
+			[
+					'可以在恶魔领域中稀有地找到（请查阅血染知书中的“The Antechamber”）',
+					' ',
+					'如果普通血命果生长时在它上面杀死一只动物，它会长成饱满血命果',
+			]
 	)
 })
